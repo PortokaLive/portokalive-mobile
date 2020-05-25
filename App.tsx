f5fd29b330c47c006779d25e8039b1f116ed762e
m3yevn <mmm.myintmyat@gmail.com>
2020-05-25 18:42:00 +0800

    wrap: redux provider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider as ReduxProvider } from "react-redux";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider } from "@ui-kitten/components";
 import { HomeScreen } from "./src/screens/HomeScreen";
+import { Store } from "./src/utils/redux/Store";
 
 const Stack = createStackNavigator();
 
 function RootStack() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" headerMode="none">
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+    <ReduxProvider store={Store}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home" headerMode="none">
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </ReduxProvider>
   );
 }
 
