7e8b754095609af2717e2d574c838b75e533acba
m3yevn <mmm.myintmyat@gmail.com>
2020-05-25 18:27:39 +0800

    🍊 change: to portokalive name

--- android/app/src/main/java/com/ishowrn/MainActivity.java
+++ android/app/src/main/java/com/ishowrn/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ishowrn;
+package com.portokalive;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "iShowRN";
+    return "PortokaLive";
   }
 }
