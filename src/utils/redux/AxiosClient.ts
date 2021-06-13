0aff9c3c8e13a3a29bd4a21fb9fb88fb3e145bd1
m3yevn <mmm.myintmyat@gmail.com>
2021-06-13 12:56:05 +0800

    update: .env and gitignore

--- src/utils/redux/AxiosClient.ts
+++ src/utils/redux/AxiosClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Config from "./Config";
+import Config from "react-native-config";
 import { getItem } from "../helpers/AsyncStorageHelper";
 
 export const getToken = (key: string) =>
